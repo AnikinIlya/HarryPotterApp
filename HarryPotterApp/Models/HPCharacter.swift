2d0a1ef3971026df05aa400fb521d1c8473eb1fe
AnikinIlya <98335973+AnikinIlya@users.noreply.github.com>
2022-10-19 16:00:10 +0400

    Project started 1. Model 2. Main storyboard

---
+++ HarryPotterApp/Models/HPCharacter.swift
@@ -0,0 +1,42 @@
+//
+//  HPCharacter.swift
+//  HarryPotterApp
+//
+//  Created by Anikin Ilya on 19.10.2022.
+//
+
+import Foundation
+
+enum House: Decodable {
+    case gryffindor
+    case ravenclaw
+    case hufflepuff
+    case slytherin
+}
+
+struct HPCharacter: Decodable {
+    let name: String
+    let alternate_names: [String]
+    let species: String
+    let gender: String
+    let house: House
+    let dateOfBirth: Date
+    let wizard: Bool
+    let ancestry: String
+    let eyeColour: String
+    let hairColour: String
+    let wand: Wand
+    let patronus: String
+    let hogwartsStudent: Bool
+    let hogwartsStaff: Bool
+    let actor: String
+    let alternate_actors: [String]
+    let alive: Bool
+    let image: String
+}
+
+struct Wand: Decodable {
+    let wood: String
+    let core: String
+    let length: Int
+}
