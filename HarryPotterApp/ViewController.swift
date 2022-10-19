40f9ced8c5373e2e454d8c08017b7e12a7999baf
AnikinIlya <98335973+AnikinIlya@users.noreply.github.com>
2022-10-19 14:39:03 +0400

    Initial Commit

---
+++ HarryPotterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HarryPotterApp
+//
+//  Created by Anikin Ilya on 19.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
